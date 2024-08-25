93716dc1bf66d597d704c087a302b624371997b8
Tran Quang Trinh <trinhtq8123@gmail.com>
2024-08-25 09:06:43 +0700

    ..

---
+++ Lab 1/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey.shade900),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("I am rich"),
+        ),body: Center(child: Image.asset("assets/diamon.jpeg"),),
+      ),
+    );
+  }
+}
